8e68ba93fed58e3e31ad1a1db4de82a4c8b18d0f
Astronix8506 <113216692+Astronix8506@users.noreply.github.com>
2024-11-06 21:11:14 -0500

    Project 7
    Updated website for mobile launch.  Upgraded menu drop down to actually work on mobile.

---
+++ public_html/csci1145/project7/javaScript.js
@@ -0,0 +1,4 @@
+function toggleDropdown() {
+    const dropdownContent = document.querySelector('.dropDownContent');
+    dropdownContent.style.display = dropdownContent.style.display === 'flex' ? 'none' : 'flex';
+}
